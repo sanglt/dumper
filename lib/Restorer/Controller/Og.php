7d3ff5c7906f746a2c3d1d4ca4e6c8ce3bb20051
Viet Coop <contact@vietcoop.com>
2013-02-20 01:34:27 +0700

    Working on drush commands.

--- lib/Restorer/Controller/Og.php
+++ lib/Restorer/Controller/Og.php
@@ -4,4 +4,12 @@
  * Wrapper class to restore the dumped og contents.
  */
 class Restorer_Controller_Og {
+  public $og;
+
+  public function __construct($og) {
+    $this->og = $og;
+  }
+
+  public function restore() {
+  }
 }
