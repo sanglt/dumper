befcb6a9dca9faf9cedb83b561561825b626f21f
Viet Coop <contact@vietcoop.com>
2013-02-20 11:24:25 +0700

    Provide default getItems() methods.

--- lib/Dumper/Content/Base/Entity.php
+++ lib/Dumper/Content/Base/Entity.php
@@ -7,13 +7,28 @@ class Dumper_Content_Base_Entity extends Dumper_Content_Base_Controller {
   public function getFields() {}
 
   /**
+   * Get items IDs.
+   *
    * @task fetch
    */
   public function getItemIds() {
-    return array();
+    $select = db_select('og_membership', 'ogm');
+    $select->condition('ogm.gid', $this->og->nid);
+    $select->condition('ogm.group_type', 'node');
+    $select->condition('ogm.entity_type', $this->entity_type);
+    $select->fields('ogm', array('etid'));
+    $ids = $select->execute()->fetchAllKeyed();
+    $ids = array_keys($ids);
+    return $ids;
   }
 
   public function queueItems() {
+    // Delete the current queue
+    $query = db_delete($this->queue_table)
+      ->condition('gid', $this->og->nid)
+      ->condition('entity_type', $this->entity_type)
+      ->execute();
+
     foreach ($this->getItemIds() as $entity_id) {
       $this->queueItem($entity_id);
     }
@@ -25,17 +40,14 @@ class Dumper_Content_Base_Entity extends Dumper_Content_Base_Controller {
    * @param int $entity_id
    */
   public function queueItem($entity_id) {
-    drush_print_r(array($this->queue_table, array(
-      'og_id' => $this->og->nid,
-      'entity_type' => $this->entity_type,
-      'entity_id' => $entity_id,
-    )));
-
-    return db_insert($this->queue_table, array(
-      'og_id' => $this->og->nid,
-      'entity_type' => $this->entity_type,
-      'entity_id' => $entity_id,
-    ))->execute();
+    return db_insert($this->queue_table)
+      ->fields(array(
+        'gid' => $this->og->nid,
+        'entity_type' => $this->entity_type,
+        'entity_id' => $entity_id,
+        'processed' => 0
+      ))
+      ->execute();
   }
 
   /**
