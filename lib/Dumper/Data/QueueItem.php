c8c3ce6b2ca939eab8dabb95cf1439edd2f635cb
Viet Coop <contact@vietcoop.com>
2013-02-20 16:15:34 +0700

    Queue Item has gid makes more sense.

--- lib/Dumper/Data/QueueItem.php
+++ lib/Dumper/Data/QueueItem.php
@@ -1,12 +1,12 @@
 <?php
 
 class Dumper_Data_QueueItem {
-  public $school;
+  public $gid;
   public $entity_type;
   public $entity_id;
 
-  public function __construct($school, $entity_type, $entity_id) {
-    $this->school = $school;
+  public function __construct($gid, $entity_type, $entity_id) {
+    $this->gid = $gid;
     $this->entity_type = $entity_type;
     $this->entity_id = $entity_id;
   }
