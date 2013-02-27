b6cf34d504c85656fb031c341a68d92efb9c510c
Pal O1.FTP <palserver@toila.net>
2013-02-27 22:54:52 +0700

    Fix #4261371315325: Comment was not dumped.

--- lib/Dumper/Content/Node.php
+++ lib/Dumper/Content/Node.php
@@ -1,4 +1,26 @@
 <?php
 
 class Dumper_Content_Node extends Dumper_Content_Base_Entity {
+  /**
+   * Node entity has comment, we need add related comment to the queue.
+   *
+   * @param Dumper_Data_QueueItem $queue_item
+   * @return boolean
+   */
+  public function preprocessQueueItem(Dumper_Data_QueueItem $queue_item) {
+    if (FALSE === parent::preprocessQueueItem($queue_item)) {
+      return FALSE;
+    }
+
+    // Queue related comments
+    $select = db_select('comment', 'c');
+    $select->condition('c.nid', $queue_item->entity_id);
+    $select->fields('c', array('cid'));
+    foreach ($select->execute()->fetchAll() as $row) {
+      if (!isset($controller)) {
+        $controller = $this->og_controller->getDataController('comment');
+      }
+      $controller->queueItem($row->cid);
+    }
+  }
 }
