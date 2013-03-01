22a656e4b8698d1f9ba8c36e3a0883d294f3ea9d
Pal O1.FTP <palserver@toila.net>
2013-03-01 07:49:36 +0700

    Also dump vocabulary.

--- lib/Dumper/Content/Taxonomy/Term.php
+++ lib/Dumper/Content/Taxonomy/Term.php
@@ -1,10 +1,12 @@
 <?php
 
 class Dumper_Content_Taxonomy_Term extends Dumper_Content_Base_Entity {
-  /**
-   * @task fetch
-   */
-  public function getItemIds() {
-    return array();
+  public function preprocessQueueItem(Dumper_Data_QueueItem $queue_item) {
+    if (FALSE === parent::preprocessQueueItem($queue_item)) {
+      return FALSE;
+    }
+
+    $controller = $this->og_controller->getDataController('taxonomy_vocabulary');
+    $controller->queueItem($this->entity->vid);
   }
 }
